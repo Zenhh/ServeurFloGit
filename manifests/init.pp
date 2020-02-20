375176eab38be2ba8e701f84697cb9e0eb925425
willy decock <w.decock@outlook.fr>
2020-02-20 12:34:26 +0100

    2ème commit
    Hi from the template commit-msg hook

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+class muppet {
+
+    group { 'kermit':
+       ensure => present,
+    }
+
+    user { 'kermit':
+      require => Group['kermit'],
+      before => File['/home/kermit'],
+      ensure => present,
+      gid => 'kermit',
+      home => "/home/kermit",
+      shell => "/bin/bash",
+    }
+
+    file { '/home/kermit':
+      ensure => directory,
+      owner => 'kermit',
+      group => 'group',
+      before => File['/home/kermit/.profile'],
+    } 
+    
+    file { "/home/kermit/.profile":
+      ensure => file,
+      content => "echo 'Hello from kermit'",
+      owner => 'kermit',
+      group => 'kermit',
+    }
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -2,16 +2,6 @@ node default {
   notify {'Hello from Puppet Server!':}
 }
 
-node toto{
-
-   package { 'python3':
-     ensure => installed,
-   }
-   package { 'python3-pip':
-     ensure => installed,
-   }
-   package { 'pygame':
-   ensure => installed,
-   provider => pip3
-   } 
+node angent-muppet {
+  include muppet
 }
